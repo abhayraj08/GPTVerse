3d11920d738f8b402a0e29b1de592ee45e4ed565
abhayraj08 <mathematics7878@gmail.com>
2026-01-16 12:50:27 +0530

    Connect with database

--- backend/server.js
+++ backend/server.js
@@ -1,40 +1,48 @@
-import express from 'express';
-import 'dotenv/config';
-import cors from 'cors';
+import express from "express";
+import "dotenv/config";
+import cors from "cors";
+import mongoose from "mongoose";
 
 const app = express();
 const PORT = 8080;
 
+// Database Connection
+const MONGO_URL = process.env.DB_URL;
+mongoose.connect(MONGO_URL)
+  .then(() => console.log('Database is connected'))
+  .catch((e) => console.log('Error', e))
+
+// Middleware
 app.use(express.json());  //lets your server read JSON data --> req.body
 app.use(cors());  // lets your frontend talk to your backend
 
+
 // https://platform.openai.com/docs/api-reference/chat?lang=node.js
-app.post('/test', async (req, res) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({
-      model: "gpt-4o-mini",
-      messages: [{
-        role: "user",
-        content: req.body.message
-      }]
-    })
-  };
+// app.post('/test', async (req, res) => {
+//   const options = {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json",
+//       "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+//     },
+//     body: JSON.stringify({
+//       model: "gpt-4o-mini",
+//       messages: [{
+//         role: "user",
+//         content: req.body.message
+//       }]
+//     })
+//   };
 
-
-  try{
-    const response = await fetch("https://api.openai.com/v1/chat/completions", options);
-    const data = await response.json();
-    // console.log(data);
-    res.send(data);
-  } catch (e) {
-    console.log("This is error : ", e);
-  }
-})
+//   try{
+//     const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+//     const data = await response.json();
+//     // console.log(data);
+//     res.send(data);
+//   } catch (e) {
+//     console.log("This is error : ", e);
+//   }
+// })
 
 
 
---
+++ backend/utils/openai.js
@@ -0,0 +1,31 @@
+import "dotenv/config";
+
+
+// https://platform.openai.com/docs/api-reference/chat?lang=node.js
+const getOpenAIAPIResponse = async (message) => {
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+        },
+        body: JSON.stringify({
+            model: "gpt-4o-mini",
+            messages: [{
+                role: "user",
+                content: message
+            }]
+        })
+    };
+
+    try {
+        const response = await fetch("https://api.openai.com/v1/chat/completions", options);
+        const data = await response.json();
+        // console.log(data);
+        return data;
+    } catch (e) {
+        console.log("This is error : ", e);
+    }
+}
+
+export default getOpenAIAPIResponse;
