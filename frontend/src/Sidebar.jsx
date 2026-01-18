c1131a0f151ebe9128b826e996af03b4b6d26820
abhayraj08 <mathematics7878@gmail.com>
2026-01-18 23:38:37 +0530

    Add sidebar

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -3,8 +3,24 @@ import "./Sidebar.css";
 export default function Sidebar() {
 
     return (
-        <div>
-            Sidebar
-        </div>
+        <section className="sidebar">
+            {/* new chat button */}
+            <button>
+                <img src="src/assets/blacklogo.png" alt="GPT logo" className="logo"/>
+                <span><i className="fa-solid fa-pen-to-square"></i></span>
+            </button>
+
+            {/* history */}
+            <ul className="history">
+                <li>thread1</li>
+                <li>thread2</li>
+                <li>thread3</li>
+            </ul>
+
+            {/* sign */}
+            <div className="sign">
+                <p>By Abhay &hearts;</p>
+            </div>
+        </section>
     )
 }
