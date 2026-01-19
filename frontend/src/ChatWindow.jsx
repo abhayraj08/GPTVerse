23877bf15fdb216474e5668fd779541404fb7b54
abhayraj08 <mathematics7878@gmail.com>
2026-01-19 23:42:11 +0530

    Functionality: Get Reply for Prompt

--- frontend/src/ChatWindow.jsx
+++ frontend/src/ChatWindow.jsx
@@ -1,12 +1,37 @@
 import "./ChatWindow.css"
 import Chat from "./Chat"
+import { useContext } from "react"
+import { MyContext } from "./MyContext"
 
 export default function ChatWindow() {
+    const { prompt, setPrompt, reply, setReply, currThreadId } = useContext(MyContext);
+    
+    const getReply = async() => {
+        const options = {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                message: prompt,
+                threadId: currThreadId
+            })
+        }
+
+        try {
+            const response = await fetch("http://localhost:8080/api/chat", options);
+            const res = await response.json();
+            console.log(res);
+            setReply(res.reply);
+        } catch(e) {
+            console.log(e);
+        }
+    }
 
     return (
         <div className="chatWindow">
             <div className="navbar">
-                <span>SigmaGPT 
+                <span>SigmaGPT
                     <div className="model">
                         4o-mini <i className="fa-solid fa-chevron-down" />
                     </div>
@@ -20,8 +45,13 @@ export default function ChatWindow() {
 
             <div className="chatInput">
                 <div className="inputBox">
-                    <input type="text" placeholder="Ask anything"/>
-                    <div id="submit"><i className="fa-solid fa-paper-plane" /></div>
+                    <input type="text"
+                        placeholder="Ask anything"
+                        value={prompt}
+                        onChange={(e) => setPrompt(e.target.value)}
+                        onKeyDown={(e) => e.key === 'Enter' ? getReply() : ''}
+                    />
+                    <div id="submit" onClick={getReply}><i className="fa-solid fa-paper-plane" /></div>
                 </div>
                 <p className="info">
                     GPTVerse can make mistakes. Check important info. See Cookie Preferences.
